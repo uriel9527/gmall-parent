4984041e7a6a796bf83026c3d0d111a4d136dd58
uriel9527 <53757830@qq.com>
2020-04-23 22:05:39 +0800

    1

---
+++ gmall-common/src/main/java/com/peng/gmall/common/constant/GmallConstants.java
@@ -0,0 +1,6 @@
+package com.peng.gmall.common.constant;
+//111111111111111111
+public class GmallConstants {
+    public static final String KAFKA_TOPIC_STARTUP = "GMALL_STARTUP";
+    public static final String KAFKA_TOPIC_EVENT = "GMALL_EVENT";
+}
